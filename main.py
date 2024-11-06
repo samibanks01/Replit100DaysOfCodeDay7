5d389be4bbae4bbc244988338e62bf74ae9f5dc7
Samuel <61188554+samibanks01@users.noreply.github.com>
2024-11-06 03:18:24 -0900

    Create main.py
    Added the solution

---
+++ main.py
@@ -0,0 +1,15 @@
+fav = input("What would you be your favorite Anime > ")
+if fav == "Batman":
+  print("You must love the shadows.")
+  pet = input("Do you have bats in your house? ")
+  if pet == "yes":
+    print("You got the fling.")
+  else: 
+    print("No you are light.")
+elif fav == "Spiderman":
+  print("Spider Bite coming up.")
+  trait = input("Did you get bitten: ")
+  if trait == "yes":
+    print("You will crawl soon.")
+else:
+  print("You're not a Die Hard Anime fan.")
